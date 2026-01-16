696b515c8cd364088acdcf9b4a05cd9a3f395ac1
prashantkrjha26 <prashantkumarjha100@gmail.com>
2026-01-16 10:19:42 +0530

    Prashant Kumar Jha Work

---
+++ ClassWork/Day4/TC_Classes.py
@@ -0,0 +1,14 @@
+class student:
+    def display(self):
+        print("This is student class")
+
+s1=student()
+s1.display()
+
+
+class calculator:
+    def add(selfself,a,b):
+        print("sum:", a+b)
+
+c= calculator()
+c.add(100,300)
