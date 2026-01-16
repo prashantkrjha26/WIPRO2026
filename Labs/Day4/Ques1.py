def875d684a7d0773607200575a9146782f290cd
prashantkrjha26 <prashantkumarjha100@gmail.com>
2026-01-16 11:15:06 +0530

    Prashant Kumar Jha Work

---
+++ Labs/Day4/Ques1.py
@@ -0,0 +1,9 @@
+# Question – Introduction to OOPs, Classes & Objects
+
+# Topics: Introduction to OOPs, Understanding Classes & Objects
+
+# Create a class Student that:
+
+# 1. Has attributes name and roll_no
+# 2. Has a method display_details() to print student information
+# 3. Create at least two objects of the class and display their details
---
+++ Labs/Day4/Ques2.py
@@ -0,0 +1,10 @@
+# Question – Parameterized Methods, Constructors & Destructors
+
+# Topics: Parameterized Methods, Constructors & Destructors
+
+# Create a class BankAccount that:
+
+# 1. Uses a parameterized constructor to initialize account_number and balance
+# 2. Implements methods deposit(amount) and withdraw(amount)
+# 3. Uses a destructor to display a message when the object is deleted
+
