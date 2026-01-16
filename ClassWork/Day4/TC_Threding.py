07a3e99142cf46db9461835b7d06b39c0598953d
prashantkrjha26 <prashantkumarjha100@gmail.com>
2026-01-16 16:48:13 +0530

    Prashant Kumar Jha Work

--- ClassWork/Day4/TC_Threding.py
+++ ClassWork/Day4/TC_Threding.py
@@ -3,8 +3,11 @@ import threading
 def task():
     print("Thred is running")
 
-t= threading.Thread(target=task)
-t.start()
-t.join()
+t1= threading.Thread(target=task)
+#t2= threading.Thread(target=task,args=("Thread2",))
+t1.start()
+#t2.start()
+t1.join()
+#t2.join()
 
 print("Main thread ends")
