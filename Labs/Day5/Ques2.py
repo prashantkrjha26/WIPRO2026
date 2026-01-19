31bf6e0d57f952668cb8272e1db147d8ec1a52fa
prashantkrjha26 <prashantkumarjha100@gmail.com>
2026-01-19 10:46:56 +0530

    Prashant Kumar Jha Work

---
+++ Labs/Day5/Ques2.py
@@ -0,0 +1,11 @@
+# Question – Polymorphism (Method & Operator Overloading)
+
+
+# Topics: Introduction to Polymorphism, Polymorphism on Operators
+
+# 1. Create a class Calculator that demonstrates method overriding
+# 2. Create another class AdvancedCalculator that overrides a
+# method from Calculator
+# 3. Implement operator overloading by overloading the + operator
+# to add two objects of a custom class
+# 4. Demonstrate polymorphism using the same method name with different behaviors
