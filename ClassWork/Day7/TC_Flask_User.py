fe77aee190d95efb0cbd775c12b2622f2d722eeb
prashantkrjha26 <prashantkumarjha100@gmail.com>
2026-01-21 15:15:21 +0530

    Prashant Kumar Jha Work

---
+++ ClassWork/Day7/TC_Flask_User.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+users = [{"id": 1, "name": "Prashant"},
+         {"id": 2, "name": "Ram"}]
+
+
+@app.route("/", methods=["Get"])
+def home():
+    return "Welcome"
+
+
+@app.route("/users", methods=["Get"])
+def get_users():
+    return jsonify(users)
+
+
+@app.route("/users/<int:user_id>", methods=["Get"])
+def get_user(user_id):
+    for user in users:
+        if user["id"] == user_id:
+            return jsonify(user)
+    return jsonify({"message": "user not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
